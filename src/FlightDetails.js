ca0f50bf5039f21f50ec80948d9791af2c1900f6
jetpackpony <jetpackpony@gmail.com>
2017-01-25 15:49:53 +0300

    Format flight details

--- src/FlightDetails.js
+++ src/FlightDetails.js
@@ -1,13 +1,34 @@
 import React from 'react';
 
+const padTime = (time) => time.toString().length > 1 ? time.toString() : `0${time}`;
+const getHoursMinutes = (time) => `${padTime(time.getHours())}:${padTime(time.getMinutes())}`
+
+const getStopsString = (stops) => {
+  let stopsString = stops.length === 0 ? "direct" : `${stops.length} stop`;
+  if (stops.length > 1) { stopsString += "s"; }
+  stopsString += ": " + stops.map((stop) => stop.Name + ", ");
+  return stopsString.substring(0, stopsString.length - 2);
+};
+
 export default function(props) {
-  const stops = props.flight.stops.length === 0 ? "direct" : `${props.flight.stops.length} stops`;
+  const dep = new Date(props.flight.departure);
+  const arr = new Date(props.flight.arrival);
+
+  let days_en_route = Math.ceil((arr - dep) / 1000 / 3600 / 24)
+  let departure = getHoursMinutes(dep);
+  let arrival = getHoursMinutes(arr);
+  if (dep.getDate() !== arr.getDate()) {
+    arrival += ` (+${days_en_route})`;
+  }
+
+  const stopsString = getStopsString(props.flight.stops);
+
   return (
     <div className="details">
       <div className="time">
-        {props.flight.departure} - {props.flight.arrival}
+        {departure} - {arrival}
       </div>
-      <div className="stops">{stops}</div>
+      <div className="stops">{stopsString}</div>
     </div>
   );
 };
