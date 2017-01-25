5454111c311a0676ab6345f1b716543a052d9b19
jetpackpony <jetpackpony@gmail.com>
2017-01-25 16:18:58 +0300

    Add styles for flight list

--- src/Flight.js
+++ src/Flight.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import FlightDetails from './FlightDetails';
+import './Flight.scss';
 
 export default function(props) {
   const flight = props.flight;
   return (
     <div className="flight">
-      <div className="image">
-        <img src={flight.carriers[0].ImageUrl} alt={flight.carriers[0].Name} />
+      <div className="left">
+        <div className="image">
+          <img src={flight.carriers[0].ImageUrl} alt={flight.carriers[0].Name} />
+        </div>
+        <FlightDetails flight={flight} />
       </div>
-      <FlightDetails flight={flight} />
-      <div className="buy-button">
-        <a href={flight.deeplink}>Buy for ${Math.ceil(flight.price)}</a>
+      <div className="right">
+        <div className="buy-button">
+          <a href={flight.deeplink}>Buy for ${Math.ceil(flight.price)}</a>
+        </div>
       </div>
     </div>
   );
